f6cf0a36ab0bfed5dff9836f152c9c55eee03f4a
fito <fito@192.168.2.3>
2016-09-25 19:03:59 -0300

    Se agrega una línea de prueba

--- ws/prueba/UneMenuGitPrueba/src/com/prueba/HolaMundo.java
+++ ws/prueba/UneMenuGitPrueba/src/com/prueba/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Hola Mundo prueba uso Git en UneMenu");
+			System.out.println("Prueba agregando cosas");
 	}
 
 }
